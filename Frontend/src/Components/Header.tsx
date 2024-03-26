f58d1707a6f5d3f9b0785aa73d1294b9e84f3520
ZaneLeBlanc <zane.m.leblanc@gmail.com>
2024-03-25 19:59:38 -0500

    various FE changes

---
+++ Frontend/src/Components/Header.tsx
@@ -0,0 +1,18 @@
+import './Header.css'
+
+function Header() {
+
+    return (
+        <>
+            <header className="header">
+                <h1>ML Intrusion Detection Tool</h1>
+                <div className="items">
+                    <h3>Test</h3>
+                    <h3>Previous Runs</h3>
+                </div>
+            </header>
+        </>
+    )
+}
+
+export default Header
--- Frontend/src/Components/PreviousRunCard.tsx
+++ Frontend/src/Components/PreviousRunCard.tsx
@@ -1,22 +1,34 @@
 import { useState } from 'react'
 import './PreviousRunCard.css'
 
-function PreviousRunCard() {
-    
+function PreviousRunCard(props) {
+    {/*Props:
+    isSelected: boolean   #this makes it orange or grey background
+    model: string         #e.g. "LCCDE"
+    f1: string            #e.g. ".98387"  (not sure how much precision we want)
+    exTime: string        #e.g. "0m 21s"
+    date: string          #e.g. "3/18/2024 @12:04pm"
+    */}
+
+    const backgroundColor = props.isSelected ? '#FFB26B' : 'D9D9D9';
     
     return (
-        <div className="wrapper">
-        <header className="Title">
-            LCCDE
-        </header>
+        <div className="wrapper" style={{backgroundColor}}>
+            <div className="title">
+                {props.model}
+            </div>
 
-        <div className="middleItems">
-            <p>F1-SCORE: </p>
-            <p>Execution Time: </p>
-        </div>
-        <div className="bottomItem">
-            <p>Date: </p>
-        </div>
+            <div className="middleItemGroup">
+                <div className="item">
+                    <p className="bolded">F1-SCORE:&nbsp;</p><p>{props.f1}</p>
+                </div>
+                <div className="item">
+                    <p className="bolded">Execution Time:&nbsp;</p><p>{props.exTime}</p>
+                </div>
+                <div className="item">
+                    <p className="bolded">Date:&nbsp;</p><p>{props.date}</p>
+                </div>
+            </div>
         
         </div>
     )
