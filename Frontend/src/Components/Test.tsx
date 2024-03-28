8e27fee4d228c8cf21c56603cb96fb10d8968497
asubichev <asubichev@gmail.com>
2024-03-28 00:50:18 -0500

    input fields created

--- Frontend/src/Components/Test.tsx
+++ Frontend/src/Components/Test.tsx
@@ -2,7 +2,28 @@ function Test() {
 
     return (
         <>
-            <h1>TEST PAGE</h1>  
+            <h1>TEST PAGE</h1>
+            <form>
+                <label>
+                    LCCDE:
+                    <input type="text" name="lccdeinput" />
+                </label>
+                <input type="submit" value="Run"/>
+            </form>
+            <form>
+                <label>
+                    MTH:
+                    <input type="text" name="lccdeinput" />
+                </label>
+                <input type="submit" value="Run"/>
+            </form>
+            <form>
+                <label>
+                    Treez:
+                    <input type="text" name="lccdeinput" />
+                </label>
+                <input type="submit" value="Run"/>
+            </form>
         </>
     )
 }
