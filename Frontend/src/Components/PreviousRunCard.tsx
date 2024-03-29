5f3aa27233fe8e9d2ae78ff40c4f7d5fd95b46cc
ZaneLeBlanc <zane.m.leblanc@gmail.com>
2024-03-28 21:40:40 -0500

    Default page set, and PreviousRuns page additional setup

--- Frontend/src/Components/PreviousRunCard.tsx
+++ Frontend/src/Components/PreviousRunCard.tsx
@@ -1,35 +1,56 @@
 import './CSS/PreviousRunCard.css'
+import React from 'react';
+import axios from 'axios';
+import { useState } from 'react';
 
 function PreviousRunCard(props: any) {
     {/*Props:
     isSelected: boolean   #this makes it orange or grey background
     model: string         #e.g. "LCCDE"
     f1: string            #e.g. ".98387"  (not sure how much precision we want)
-    exTime: string        #e.g. "0m 21s"
+    run_ID: string        #e.g. "0001"
     date: string          #e.g. "3/18/2024 @12:04pm"
     */}
 
-    const backgroundColor = props.isSelected ? '#FFB26B' : 'D9D9D9';
+    console.log("run_ID: " + props.run_ID + "      isSelected: " + props.isSelected);
+
+    const handleFetchRun = async (id: string) => {
+    try {
+        console.log("inside handleFetchRun");
+        props.onClick();
+        // TODO: figure out from @mlandauro what endpoint to use, and what format to pass request in
+        {console.log("card is sending a response")}
+        const response = await axios.post('http://localhost:5000/fetchRun', {QueryResultWithParams: {id}});
+        
+        {/*setTreeResponse(response.data.result);*/}
+    } catch (error) {
+        console.error('Error sending response: ', error);
+    }
+    return;
+    }
     
     return (
-        <div className="wrapper" style={{backgroundColor}}>
-            <div className="title">
-                {props.model}
+        <button className="wrapper" style={{backgroundColor: props.isSelected ? '#FFB26B' : '#D9D9D9'}} onClick={props.onClick} >
+            <div className="top-section">
+                <div className="title">
+                    {props.model}
+                </div>
+                <div className="date">
+                    <p>{props.date}</p>
+                </div>
             </div>
 
             <div className="middleItemGroup">
                 <div className="item">
-                    <p className="bolded">F1-SCORE:&nbsp;</p><p>{props.f1}</p>
+                    <p>F1:&nbsp;</p><p>{props.f1}</p>
                 </div>
                 <div className="item">
-                    <p className="bolded">Execution Time:&nbsp;</p><p>{props.exTime}</p>
+                    <p>Run ID:&nbsp;</p><p>{props.run_ID}</p>
                 </div>
-                <div className="item">
-                    <p className="bolded">Date:&nbsp;</p><p>{props.date}</p>
-                </div>
+                
             </div>
         
-        </div>
+        </button>
     )
 }
 
--- Frontend/src/Components/PreviousRuns.tsx
+++ Frontend/src/Components/PreviousRuns.tsx
@@ -8,39 +8,102 @@ function PreviousRuns() {
     {/* Goal here is to collect an array of card info from python,
         and then with that array we will create another array of 
         <PreviousRunCard /> components that pass in the info as props*/}
-    const [cardsInfo, setCardsInfo] = useState([]);
+
+    const [flowStarted, setFlowStarted] = useState(false);
+
+    const [selectedId, setSelectedId] = useState("-1");
+
+    const handleSelection = (newSelectedId: any) => {
+        console.log("inside handleSelection function")
+        setFlowStarted(true); {/*makes the initial text go away */}
+    
+        // Create a copy of the previousRunCards array
+        const newArray = previousRunCards.map(item => {
+            //color new
+            if (item.run_ID === newSelectedId) {
+                item.isSelected = true;
+                return item;
+            }
+            //uncolor last if not initial color:
+            if (item.run_ID === selectedId) {
+                item.isSelected = false;
+                return item;
+            }
+            return item;
+            
+        })
+        
+        // Update the state with the new array
+        setSelectedId(newSelectedId); 
+        setPreviousRunCards(newArray);
+    };
 
 
     const fetchCards = async () => {
         try {
           // Perform your fetch request here
-          const response = await fetch('history/cards');
+          const response = await fetch('http://localhost:5000/CardsData');
           const data = await response.json();
           // Assuming your data is an array of cards
-          setCardsInfo(data);
+          setPreviousRunCards(data);
         } catch (error) {
-          console.error('Error fetching data:', error);
+          console.error('Error fetching card data:', error);
         }
       };
 
+    const [previousRunCards, setPreviousRunCards] = useState([
+    //placeholder data until we can get the request working
+    { 
+        isSelected: false, 
+        model: "LCCDE", 
+        f1: "0.99", 
+        run_ID: "2891", 
+        date: "3/18/2024 @12:04pm" 
+    },
+    { 
+        isSelected: false, 
+        model: "MTH", 
+        f1: "0.91", 
+        run_ID: "0001", 
+        date: "3/17/2024 @12:02pm" 
+    },
+    { 
+        isSelected: false, 
+        model: "Tree-Based", 
+        f1: "0.92", 
+        run_ID: "0002", 
+        date: "3/16/2024 @12:02pm" 
+    }
+    // Add more cards as needed
+    ]);
+
+
     return (
         <div className="container">
-            
             <div className="sidebar">
-
-                <PreviousRunCard 
-                    isSelected={false} 
-                    model="sample"
-                    f1=".98387"
-                    exTime= "0m 21s"
-                    date="3/18/2024 @12:04pm"
-                />
-
-
+                {/* Map over the array of PreviousRunCard data and render each card */}
+                {previousRunCards.map((card, index) => (
+                    <PreviousRunCard 
+                        key={index}
+                        isSelected={card.isSelected} 
+                        model={card.model}
+                        f1={card.f1}
+                        run_ID={card.run_ID}
+                        date={card.date}
+                        // Pass a callback function with the index to handleSelection
+                        onClick={() => handleSelection(card.run_ID)}
+                    />
+                ))}
             </div>
 
             <div className="page">
+
+            {flowStarted ? (
+                <Result /> // placeholder, here we will render a specific run's parameters and result as well as a re-run button
+            ) : (
                 <h3>Select a record to view details</h3>
+            )}
+                
             </div>
         </div>
     )
