e35b3fb31bdfd4e55f4d97c9fe094f8e3f762b19
asubichev <asubichev@gmail.com>
2024-04-07 20:29:53 -0500

    tree separated

--- Frontend/src/Components/ModelPages/PageTree.tsx
+++ Frontend/src/Components/ModelPages/PageTree.tsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import {useState} from 'react'
+import axios from 'axios'
 
-const PageTree = () => {
-  return <div>This is Page 3</div>;
-};
+function PageTree() {
+    const [treeRequest, setTreeRequest] = useState('and everywhere');
+    const [treeResponse, setTreeResponse] = useState('');
 
-export default PageTree;
+    const sendTreeParams = async () => {
+        try {
+            const response = await axios.post('http://localhost:5000/runTree', {code: treeRequest});
+
+            setTreeResponse(response.data.result);
+        } catch (error) {
+            console.error('Error sending response: ', error);
+        }
+    }
+
+    return(
+        <div>
+            <h1>RUN TREE-BASED</h1>
+            <div className="testSection">
+                <textarea value={treeRequest} onChange={(e) =>
+                setTreeRequest(e.target.value)}/>
+                <button className="runbt" onClick={sendTreeParams}>Run Tree-Based</button>
+            </div>
+            <div>Result: {treeResponse}</div>
+        </div>
+    )
+}
+
+export default PageTree
