24224dded558f4032505b7b107b17afd3f9b52c5
asubichev <asubichev@gmail.com>
2024-04-07 20:16:21 -0500

    lccde separated

--- Frontend/src/Components/ModelPages/PageLCCDE.tsx
+++ Frontend/src/Components/ModelPages/PageLCCDE.tsx
@@ -1,7 +1,56 @@
 import React from 'react';
+import { useState } from "react"
+import axios from 'axios';
 
-const PageLCCDE = () => {
-  return <div>This is Page 1</div>;
-};
+function PageLCCDE() {
+    const[lccdeRequest, setLccdeRequest] = useState('');
+    const[lccdeResponse, setLccdeResponse] = useState('');
 
-export default PageLCCDE;
+    const sendLCCDEParams = async () => {
+        try {
+            
+            const response = await axios.post('http://localhost:5000/runlccde', {
+                "model_req": {
+                  "dataset_name": "",
+                  "XGB": {
+                    "n_estimators": "",
+                    "max_depth": "",
+                    "learning_rate": ""
+                  },
+                  "LightGBM": {
+                    "num_iterations": "",
+                    "max_depth": "",
+                    "learning_rate": "",
+                    "num_leaves": "",
+                    "boosting_type": ""
+                  },
+                  "CatBoost": {
+                    "n_estimators": "",
+                    "max_depth": "",
+                    "learning_rate": ""
+                  }
+                }
+              });
+
+            setLccdeResponse(response.data);
+            console.log("setLCCDE");
+            console.log(response);
+        } catch (error) {
+            console.error('Error sending response: ', error);
+        }
+    }
+
+    return(
+        <div>
+            <h1>RUN LCCDE</h1>
+            <div className="testSection">
+                <textarea value={lccdeRequest} onChange={(e) =>
+                setLccdeRequest(e.target.value)}/>
+                <button className="runbt" onClick={sendLCCDEParams}>Run LCCDE</button>
+                </div>
+            <div>Result: {lccdeResponse}</div>
+        </div>
+    )
+}
+
+export default PageLCCDE
