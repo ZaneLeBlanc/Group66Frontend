f83985ee57fc7c3ded2b4759b8fd9c1a34917795
asubichev <asubichev@gmail.com>
2024-04-07 20:29:47 -0500

    mth separated

--- Frontend/src/Components/ModelPages/PageMTH.tsx
+++ Frontend/src/Components/ModelPages/PageMTH.tsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import {useState} from 'react'
+import axios from 'axios'
 
-const PageMTH = () => {
-  return <div>This is Page 2</div>;
-};
+function PageMTH() {
+    const [mthRequest, setMthRequest] = useState('and here');
+    const [mthResponse, setMthResponse] = useState('');
 
-export default PageMTH;
+    const sendMTHParams = async () => {
+        try {
+            const response = await axios.post('http://localhost:5000/runMth', {code: mthRequest});
+
+            setMthResponse(response.data.result);
+        } catch (error) {
+            console.error('Error sending response: ', error);
+        }
+    }
+
+    return(
+        <div>
+            <h1>RUN MTH</h1>
+            <div className="testSection">
+                <textarea value={mthRequest} onChange={(e) =>
+                setMthRequest(e.target.value)}/>
+                <button className="runbt" onClick={sendMTHParams}>Run MTH</button>
+            </div>
+            <div>Result: {mthResponse}</div>
+        </div>
+    )
+}
+
+export default PageMTH
